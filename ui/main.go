8c23f9411ddc6b6652bfefe59ff24154a6f0d9ae
alyarctiq <aly.khimji@artiq.ca>
2019-11-23 12:58:45 -0500

    initial

---
+++ ui/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type microservicedata struct {
+	Data string `json:"Data"`
+	Logo string `json:"Logo"`
+	Svc  string `json:"Svc"`
+}
+
+func beerme(w http.ResponseWriter, r *http.Request) {
+	req, err := http.NewRequest("GET", "http://go-web-beer", nil)
+	if err != nil {
+		//handle err
+	}
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Fprintf(w, "<p align='center'>Doh The Backend Appears Down!! </p>")
+		fmt.Fprintf(w, "<p align='center'></p>")
+		w.Header().Set("Content-Type", "image/jpeg")
+		fmt.Fprintf(w, "<p align='center'><img src='./home-nobackend.gif' alt='gopher' style='width:320px;height:320px;'></p>")
+		return
+	}
+
+	body, readErr := ioutil.ReadAll(resp.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+	beer := microservicedata{}
+	jsonErr := json.Unmarshal(body, &beer)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	defer resp.Body.Close()
+
+	t, err := template.ParseFiles("templates/beer.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	t.Execute(w, beer)
+}
+
+func dadjoke(w http.ResponseWriter, r *http.Request) {
+	req, err := http.NewRequest("GET", "http://go-web-dadjoke", nil)
+	if err != nil {
+		// handle err
+	}
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Fprintf(w, "<h2><p align='center'>Doh The Backend Appears Down!! </p></h2>")
+		fmt.Fprintf(w, "<p align='center'></p>")
+		w.Header().Set("Content-Type", "image/jpeg")
+		fmt.Fprintf(w, "<p align='center'><img src='./home-nobackend.gif' alt='gopher' style='width:320px;height:320px;'></p>")
+		return
+	}
+
+	body, readErr := ioutil.ReadAll(resp.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+	beer := microservicedata{}
+	jsonErr := json.Unmarshal(body, &beer)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	defer resp.Body.Close()
+
+	t, err := template.ParseFiles("templates/dadjoke.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	t.Execute(w, beer)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/beer", beerme)
+	http.HandleFunc("/joke", dadjoke)
+	http.Handle("/", http.FileServer(http.Dir("./html")))
+}
+
+func main() {
+	fmt.Println("Go Web App Started")
+	setupRoutes()
+	http.ListenAndServe(":3000", nil)
+}
