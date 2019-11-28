bf0359f21845e8a5c8b9cd1219aaa3b72761428f
alyarctiq <aly.khimji@artiq.ca>
2019-11-28 16:36:29 -0500

    simpsons

--- ui/main.go
+++ ui/main.go
@@ -50,6 +50,40 @@ func beerme(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, beer)
 }
 
+func simpsons(w http.ResponseWriter, r *http.Request) {
+	req, err := http.NewRequest("GET", "http://go-web-beer", nil)
+	if err != nil {
+		//handle err
+	}
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Fprintf(w, "<p align='center'>Doh The Backend Appears Down!! </p>")
+		fmt.Fprintf(w, "<p align='center'></p>")
+		w.Header().Set("Content-Type", "image/jpeg")
+		fmt.Fprintf(w, "<p align='center'><img src='images/home-nobackend.gif' alt='HomeNoooo' style='width:320px;height:320px;'></p>")
+		return
+	}
+
+	body, readErr := ioutil.ReadAll(resp.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+	beer := microservicedata{}
+	jsonErr := json.Unmarshal(body, &beer)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	defer resp.Body.Close()
+
+	t, err := template.ParseFiles("templates/beer.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	t.Execute(w, beer)
+}
 func dadjoke(w http.ResponseWriter, r *http.Request) {
 	req, err := http.NewRequest("GET", "http://go-web-dadjoke", nil)
 	if err != nil {
@@ -88,6 +122,7 @@ func dadjoke(w http.ResponseWriter, r *http.Request) {
 func setupRoutes() {
 	http.HandleFunc("/beer", beerme)
 	http.HandleFunc("/joke", dadjoke)
+	http.HandleFunc("/simpsons", simpsons)
 	http.Handle("/", http.FileServer(http.Dir("./html")))
 }
 
